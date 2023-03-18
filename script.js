3350945f1a90116321a6633c4a3dee7032b5017c
uttamshukla259 <117875151+uttamshukla259@users.noreply.github.com>
2023-03-18 20:02:54 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,17 +1,28 @@
 //your code here
 
-let bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Rolling Stones', 'The Who', 'Aerosmith', 'Queen'];
+const bandNames = ['The Beatles', 'Led Zeppelin', 'Pink Floyd', 'The Who', 'Aerosmith', 'Rolling Stones', 'Queen', 'Anthrax', 'Black Sabbath'];
 
-// Remove articles from the band names
-bandNames = bandNames.map(name => name.replace(/^(the|a|an)\s+/i, ''));
+		function sortBands(bands) {
+			const articles = ['the', 'a', 'an'];
+			const sortedBands = bands.sort(function(a, b) {
+				const nameA = a.toLowerCase().replace(/^(the|an|a)\s+/, '');
+				const nameB = b.toLowerCase().replace(/^(the|an|a)\s+/, '');
+				if (nameA < nameB) {
+					return -1;
+				}
+				if (nameA > nameB) {
+					return 1;
+				}
+				return 0;
+			});
+			return sortedBands;
+		}
 
-// Sort the band names in lexicographic order
-bandNames.sort();
+		const sortedBands = sortBands(bandNames);
 
-// Display the sorted band names in a list
-const bandList = document.getElementById('band');
-bandNames.forEach(name => {
-  const li = document.createElement('li');
-  li.textContent = name;
-  bandList.appendChild(li);
-});
+		const bandList = document.getElementById("band");
+		sortedBands.forEach(function(band) {
+			const li = document.createElement("li");
+			li.appendChild(document.createTextNode(band));
+			bandList.appendChild(li);
+		});
